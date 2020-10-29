1d37588d558edf5d8e938edce582de60c55456cc
PeiYuann <1801056@sit.singaporetech.edu.sg>
2020-10-29 15:34:27 +0800

    Revert "Revert "Implement Planner UI and Firebase connection""
    This reverts commit 650a8d5782414005c89b9f21100a13c468ebbfa1.

--- app/src/main/java/edu/singaporetech/ict3104/project/CommuteMainActivity.java
+++ app/src/main/java/edu/singaporetech/ict3104/project/CommuteMainActivity.java
@@ -10,12 +10,12 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+public class CommuteMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.commute_activity_main);
 
         final BottomNavigationView navView = findViewById(R.id.nav_view);
         final NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
