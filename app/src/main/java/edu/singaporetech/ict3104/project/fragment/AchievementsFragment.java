ef32c853b44a1b9bfe998654dd50dc3db599debc
PeiYuann <1801056@sit.singaporetech.edu.sg>
2020-11-02 16:48:21 +0800

    T08-67 integration with fb

--- app/src/main/java/edu/singaporetech/ict3104/project/fragment/AchievementsFragment.java
+++ app/src/main/java/edu/singaporetech/ict3104/project/fragment/AchievementsFragment.java
@@ -92,7 +92,11 @@ public class AchievementsFragment extends Fragment {
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                Long getAge = document.getLong("Age");
+
+                                Log.d("Planner", document.getId() + " => " + document.getData());
+                                System.out.println("PLANNER : " + document.getLong("Age"));
+                                Log.d("PlannerS", document.getId() + " => " + document.getLong("Age"));
                             }
                         } else {
                             Log.w(TAG, "Error getting documents.", task.getException());
