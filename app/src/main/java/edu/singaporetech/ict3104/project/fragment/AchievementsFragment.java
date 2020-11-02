930bbc0db66d40501577be93a0f32ee59752fac4
zhengyu96 <47882185+zhengyu96@users.noreply.github.com>
2020-11-02 16:12:11 +0800

    add read collection (incomplete)

--- app/src/main/java/edu/singaporetech/ict3104/project/fragment/AchievementsFragment.java
+++ app/src/main/java/edu/singaporetech/ict3104/project/fragment/AchievementsFragment.java
@@ -2,6 +2,7 @@ package edu.singaporetech.ict3104.project.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,8 +16,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.w3c.dom.Text;
 
@@ -29,6 +39,8 @@ import java.util.List;
 import edu.singaporetech.ict3104.project.LoginActivity;
 import edu.singaporetech.ict3104.project.R;
 
+import static android.content.ContentValues.TAG;
+
 public class AchievementsFragment extends Fragment {
 
     private TextView textViewSortFN;
@@ -36,6 +48,9 @@ public class AchievementsFragment extends Fragment {
     private TextView textViewSortFA;
     private TextView textViewSortFG;
     private TextView textViewSortFM;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private String email;
 //    private ArrayList<feature> featureList;
 //    private FeatureListAdapter adapter;
 
@@ -45,11 +60,57 @@ public class AchievementsFragment extends Fragment {
 
     @Nullable
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser == null) {
+            Toast.makeText(getContext(), "User not logged in.", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(getContext(), LoginActivity.class));
+            return;
+        }
+
+        email = firebaseUser.getEmail();
+
+        if (email == null) {
+            Toast.makeText(getContext(), "Unexpected error with the user.", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(getContext(), LoginActivity.class));
+        }
+
+
+        //Toast.makeText(getContext(),commuteType,Toast.LENGTH_LONG).show();
+
+        //read commute method from user in fb
+
+    }
+    public void getFeatures(){
+        db.collection("Survey")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+    }
+
+
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate((R.layout.fragment_achievements), container, false);
         ListView mListView = (ListView) view.findViewById(R.id.planner_listview);
+        //read from database
+        getFeatures();
 
+        //
         //Create the feature objects
         feature feature1 = new feature("lamp", 5, 21, 'M', 'W');
         feature feature2 = new feature("staircase", 5, 21, 'M', 'W');
