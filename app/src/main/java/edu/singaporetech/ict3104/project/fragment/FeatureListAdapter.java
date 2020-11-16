0f7ac50051fa39f0971bf3b5725fd1a1415a611b
Yong Jian Ming <jianming1993@gmail.com>
2020-11-16 17:22:08 +0800

    Optimize

--- app/src/main/java/edu/singaporetech/ict3104/project/fragment/FeatureListAdapter.java
+++ app/src/main/java/edu/singaporetech/ict3104/project/fragment/FeatureListAdapter.java
@@ -1,26 +1,22 @@
 package edu.singaporetech.ict3104.project.fragment;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import edu.singaporetech.ict3104.project.R;
 
 public class FeatureListAdapter extends ArrayAdapter<feature> {
 
     private static final String TAG = "FeatureListAdapter";
-
-    private FragmentActivity mContext;
     int mResource;
+    private FragmentActivity mContext;
 
     public FeatureListAdapter(@NonNull FragmentActivity context, int resource, @NonNull ArrayList<feature> objects) {
         super(context, resource, objects);
@@ -28,7 +24,7 @@ public class FeatureListAdapter extends ArrayAdapter<feature> {
         mResource = resource;
     }
 
-    public View getView(int position, View convertView, ViewGroup parent){
+    public View getView(int position, View convertView, ViewGroup parent) {
         //get the feature information
         String name = getItem(position).getFeature_name();
         int rating = getItem(position).getFeature_rating();
@@ -37,7 +33,7 @@ public class FeatureListAdapter extends ArrayAdapter<feature> {
         char method = getItem(position).getUser_method();
         //create the feature object with the info
 
-        feature feature = new feature(name,rating,age,gender,method);
+        feature feature = new feature(name, rating, age, gender, method);
         LayoutInflater inflater = LayoutInflater.from(mContext);
         if (convertView == null) {
             convertView = inflater.inflate(mResource, parent, false);
